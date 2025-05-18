5160d67bc5d13059ec245f19a3a2c78372bba3e7
Binh Nguyen <quocbinh0612@gmail.com>
2025-05-18 21:36:47 +0700

    Finish handlebar js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const expressHbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "/html"))
+app.use(express.static(__dirname + "/html"));
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/html/index.htm"))
+app.engine(
+    "hbs", 
+    expressHbs.engine({
+        extname: "hbs",
+        defaultLayout : "layout",
+        layoutsDir: __dirname + "/views/layouts",
+        partialsDir: __dirname + "/views/partials",
+    })
+);
+
+app.set("view engine", "hbs");
+
+// CONFIG to read post req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended : false}));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.use("/task1", require("./routes/task1Router"));
+app.use("/task2", require("./routes/task2Router"));
+app.use("/task3", require("./routes/task3Router"));
+app.use("/task4", require("./routes/task4Router"));
 
 app.listen(port, () => console.log(`Application listen on port: ${port}`));
